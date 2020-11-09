388a23486f1bf60b9cbf8a2990c50f2455d99ad6
Alkey <Sirj777sirj@gmail.com>
2020-11-09 17:34:13 +0200

    fix comments

--- src/test/java/sboot/example/CsvReaderUtilImplTest.java
+++ src/test/java/sboot/example/CsvReaderUtilImplTest.java
@@ -1,6 +1,7 @@
 package sboot.example;
 
 import com.opencsv.CSVReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
@@ -8,17 +9,16 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.ResourceUtils;
 import sboot.example.util.CsvReaderUtil;
 import sboot.example.util.CsvReaderUtilImpl;
 
 
 public class CsvReaderUtilImplTest {
-    private static final String CORRECT_PATH = "src/test/resources/test.csv";
-    private static final String INCORRECT_PATH = "src/test/resources/correct/path.csv";
     private static final List<String[]> CORRECT_LIST =
             List.of(new String[]{"Id", "ProductId", "UserId", "ProfileName",
-                    "HelpfulnessNumerator", "HelpfulnessDenominator", "Score",
-                    "Time", "Summary", "Text"},
+                            "HelpfulnessNumerator", "HelpfulnessDenominator", "Score",
+                            "Time", "Summary", "Text"},
                     new String[]{"1", "B001E4KFG0", "A3SGXH7AUHU8GW", "delmartian", "1",
                             "1", "5", "1303862400",
                             "Good Quality Dog Food", "I have bought several of the Vitality "
@@ -30,7 +30,8 @@ public class CsvReaderUtilImplTest {
     @Test
     public void readFileWithCorrectPath() throws FileNotFoundException {
         List<String[]> result;
-        CsvReaderUtil reader = new CsvReaderUtilImpl(new CSVReader(new FileReader(CORRECT_PATH)));
+        File file = ResourceUtils.getFile("classpath:test.csv");
+        CsvReaderUtil reader = new CsvReaderUtilImpl(new CSVReader(new FileReader(file)));
         result = reader.read();
         assertEquals(10, result.get(0).length);
     }
@@ -38,12 +39,14 @@ public class CsvReaderUtilImplTest {
     @Test
     public void readFileWithIncorrectPath() {
         assertThrows(FileNotFoundException.class, () ->
-                new CsvReaderUtilImpl(new CSVReader(new FileReader(INCORRECT_PATH))));
+                new CsvReaderUtilImpl(new CSVReader
+                        (new FileReader(ResourceUtils.getFile("classpath:tet.csv")))));
     }
 
     @Test
     public void chekForCorrectReadFile() throws FileNotFoundException {
-        CsvReaderUtil reader = new CsvReaderUtilImpl(new CSVReader(new FileReader(CORRECT_PATH)));
+        File file = ResourceUtils.getFile("classpath:test.csv");
+        CsvReaderUtil reader = new CsvReaderUtilImpl(new CSVReader(new FileReader(file)));
         List<String[]> read = reader.read();
         for (int i = 0; i < read.size(); i++) {
             assertArrayEquals(CORRECT_LIST.get(i), read.get(i));
